6424075e21156770cd1d3d0e7c5bf887d09ff398
benf549 <benjaminpfry@gmail.com>
2023-12-12 21:58:00 -0500

    Implement dataset sampler.

--- generate_sequence_clusters.py
+++ generate_sequence_clusters.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import os
+import torch
 import subprocess
 from pathlib import Path
-from utils.dataset import UnclusteredProteinDataset, ClusteredDatasetSampler
+from utils.dataset import ProteinAssemblyDataset, get_list_of_all_paths, ClusteredDatasetSampler
 from utils.constants import MAX_PEPTIDE_LENGTH
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+
 
 def chain_identifier_to_polymer_sequence(protein_data, pdb_code):
     output = {}
@@ -15,34 +18,34 @@ def chain_identifier_to_polymer_sequence(protein_data, pdb_code):
     return output
 
 
-def write_polymer_seqs_to_fasta(params):
-    all_sequences = {}
-    protein_dataset = UnclusteredProteinDataset(params)
+def generate_mmseqs_clusters(all_sequences, params):
+    with open(params['fasta_output_path'], 'w') as f:
+        for identifier, sequence in all_sequences.items():
+            f.write(f'>{identifier}\n{sequence}\n')
+    
+    # Cluster at 30% sequence identity.
+    # mmseqs easy-cluster fasta.txt cluster30test tmp30test --min-seq-id 0.3 -c 0.5 --cov-mode 5 --cluster-mode 3
+    output_path = params["fasta_output_path"]
+    clustering_output_path_prefixes = os.path.join(params["clustering_output_path"], params['clustering_output_prefix'])
+    mmseq_metadata_output_path = os.path.join(params["clustering_output_path"], "mmseq_tmp_dir")
+    subprocess.run(f'mmseqs easy-cluster {output_path} {clustering_output_path_prefixes} {mmseq_metadata_output_path} --min-seq-id 0.3 -c 0.5 --cov-mode 5 --cluster-mode 3', shell=True)
 
-    # sampler = ClusteredDatasetSampler(protein_dataset, params)
-    # dataloader = DataLoader(protein_dataset, batch_size=10_000, sampler=sampler)
-    # for data in dataloader:
-    #     print(data)
+def write_polymer_seqs_to_fasta(params):
+
+    all_sequences = {}
+    protein_dataset = ProteinAssemblyDataset(params)
 
     for protein_data, pdb_code in protein_dataset:
         polymer_sequence_map = chain_identifier_to_polymer_sequence(protein_data, pdb_code.replace('.pt', ''))
         all_sequences.update(polymer_sequence_map)
 
-    # with open(params['fasta_output_path'], 'w') as f:
-    #     for identifier, sequence in all_sequences.items():
-    #         f.write(f'>{identifier}\n{sequence}\n')
-    
-    # # Cluster at 30% sequence identity.
-    # # mmseqs easy-cluster fasta.txt cluster30test tmp30test --min-seq-id 0.3 -c 0.5 --cov-mode 5 --cluster-mode 3
-    # output_path = params["fasta_output_path"]
-    # clustering_output_path_prefixes = os.path.join(params["clustering_output_path"], params['clustering_output_prefix'])
-    # mmseq_metadata_output_path = os.path.join(params["clustering_output_path"], "mmseq_tmp_dir")
-    # subprocess.run(f'mmseqs easy-cluster {output_path} {clustering_output_path_prefixes} {mmseq_metadata_output_path} --min-seq-id 0.3 -c 0.5 --cov-mode 5 --cluster-mode 3', shell=True)
+    generate_mmseqs_clusters(all_sequences, params)
 
 
 if __name__ == "__main__":
     params = {
-        'dataset_path': '/scratch/bfry/torch_bioasmb_dataset/aa/',
+        'debug': (debug := True),
+        'dataset_path': '/scratch/bfry/torch_bioasmb_dataset' + '/aa' if debug else '',
         'clustering_output_prefix': 'torch_bioas_cluster30',
         'clustering_output_path': (output_path := '/scratch/bfry/bioasmb_dataset_sequence_clustering/'),
         'fasta_output_path': os.path.join(output_path, 'all_torch_polymer_sequences.fasta')
--- utils/dataset.py
+++ utils/dataset.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from typing import Tuple, Dict
+from typing import Tuple, Dict, List, Union
 from collections import defaultdict
 
 import torch
@@ -11,19 +11,39 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, Sampler
 
-def create_chain_to_cluster_mapping(df: pd.DataFrame) -> Dict[str, str]:
+
+def get_list_of_all_paths(path: str) -> list:
+    """
+    Recursively get a list of all paths of pytorch files in a directory.
+    """
+    all_paths = []
+    for subdir_or_files in os.listdir(path):
+        path_to_subdir_or_file = os.path.join(path, subdir_or_files)
+        if path_to_subdir_or_file.endswith('.pt'):
+            all_paths.append(path_to_subdir_or_file)
+        elif os.path.isdir(path_to_subdir_or_file):
+            all_paths.extend(get_list_of_all_paths(path_to_subdir_or_file))
+    return all_paths
+
+
+def create_chain_to_cluster_mapping(df: pd.DataFrame, params: dict) -> Dict[str, str]:
     chain_to_cluster = {}
     for _, row in df.iterrows():
         chain = row['chain']
+        if params['debug']:
+            if not chain.split('_')[0][1:3] == params['dataset_path'].rsplit('/', 1)[1]:
+                continue
         cluster_representative = row['cluster_representative']
         chain_to_cluster[chain] = cluster_representative
     return chain_to_cluster
+
 
 def invert_dict(d: dict) -> dict:
     clusters = defaultdict(list)
     for k, v in d.items():
         clusters[v].append(k)
     return dict(clusters)
+
 
 def chain_list_to_protein_chain_dict(chain_list: list) -> dict:
     """
@@ -38,42 +58,121 @@ def chain_list_to_protein_chain_dict(chain_list: list) -> dict:
 
     return dict(bioasmb_list)
 
-class ClusteredDatasetSampler(Sampler):
-    def __init__(self, dataset, params):
 
+def get_complex_len(complex_data: dict) -> int:
+    return sum([x['size'] for x in complex_data.values()])
+
+def collate_sampler_data(data: list):
+    print(data)
+    raise NotImplementedError
+
+class ClusteredDatasetSampler(Sampler):
+    """
+    Samples a single protein complex from precomputed mmseqs clusters.
+    """
+    def __init__(self, dataset, params):
         # The unclustered dataset where each complex/assembly is a single index.
         self.dataset = dataset
+        self.batch_size = params['batch_size']
 
         # Load the cluster data.
+        print("Loading sequence clusters.")
         df = pd.read_csv(os.path.join(params['clustering_output_path'], f"{params['clustering_output_prefix']}_cluster.tsv"), sep='\t', header=None)
         df.columns = ['cluster_representative', 'chain']
 
         # Maps a given pdb_code+chain to its representative cluster.
-        self.chain_to_cluster = create_chain_to_cluster_mapping(df)
+        self.chain_to_cluster = create_chain_to_cluster_mapping(df, params)
 
         # Maps sequence cluster to number of chains.
         self.cluster_to_chains = invert_dict(self.chain_to_cluster)
 
-    def __len__(self):
-        return len(self.cluster_to_chains)
+        # Sample the first epoch.
+        self.curr_samples = []
+        self.sample_clusters()
 
-    def __iter__(self):
+    def get_curr_sample_len(self) -> int:
+        return sum(self.dataset.index_to_complex_size[x] for x in self.curr_samples)
+
+    def __len__(self) -> int:
+        return (self.get_curr_sample_len() + self.batch_size - 1) // self.batch_size
+    
+    def sample_clusters(self):
+        self.curr_samples = []
         # Loop over mmseqs cluster and list of chains for that cluster.
         for cluster, chains in self.cluster_to_chains.items():
+            # Convert list of all chains/pdbs/assemblies to a dictionary mapping pdb code to a 
+            # list of assemblies and chains in the current cluster.
             pdb_to_assembly_chains_map = chain_list_to_protein_chain_dict(chains)
+
+            # Sample from the PDBs with the desired chain cluster.
             sampled_pdb = np.random.choice(list(pdb_to_assembly_chains_map.keys()))
+
+            # Given the PDB to sample from sample an assembly and chain for training.
             sampled_assembly_and_chains = np.random.choice(pdb_to_assembly_chains_map[sampled_pdb])
-            sampled_pdb = '_'.join([sampled_pdb, sampled_assembly_and_chains])
-            print(cluster, '->', sampled_pdb)
+  
+            # Reform the string representation of the sampled pdb_assembly-seg-chain.
+            chain_key = '_'.join([sampled_pdb, sampled_assembly_and_chains])
 
-        raise NotImplementedError
+            # Yield the index of the sampled pdb_assembly-seg-chain.
+            self.curr_samples.append(self.dataset.chain_key_to_index[chain_key])
+        
+    def __iter__(self):
+        # Sort the samples by size.
+        curr_samples_tensor = torch.tensor(self.curr_samples)
+        sizes = torch.tensor([self.dataset.index_to_complex_size[x] for x in self.curr_samples])
+    
+        # Yield the indexes in the order of the sorted sizes.
+        outputs = []
+        for batch in torch.chunk(curr_samples_tensor[torch.argsort(sizes)], len(self)):
+            outputs.append(batch.tolist())
+        np.random.shuffle(outputs)
+    
+        for batch in outputs:
+            yield batch
+
+        # Resample for the next epoch.
+        self.sample_clusters()
 
 
-class UnclusteredProteinDataset(Dataset):
+
+class UnclusteredProteinChainDataset(Dataset):
+    def __init__(self, params):
+        self.pdb_code_to_complex_data = {} # Maps from pdb_code to protein complex/bioassembly data.
+        self.chain_key_to_index = {} # Maps from unique index to chain_key
+        self.index_to_complex_size = {}
+        idx = 0
+        for path_to_data in tqdm(get_list_of_all_paths(params['dataset_path']), desc='Loading protein dataset.'):
+            pdb_prefix = path_to_data.rsplit('/', 1)[1].replace('.pt', '')
+
+            # Load the protein complex from disk, store with pdb_prefix as key.
+            protein_data = torch.load(path_to_data)
+            self.pdb_code_to_complex_data[pdb_prefix] = protein_data
+
+            # Loop over chains and assign a unique index to chain_key
+            for chain_key, chain_data in protein_data.items():
+                chain_key = '-'.join([pdb_prefix] + list(chain_key))
+                self.chain_key_to_index[chain_key] = idx
+                self.index_to_complex_size[idx] = get_complex_len(protein_data)
+                idx += 1
+        self.index_to_chain_key = {x: y for y,x in self.chain_key_to_index.items()}
+
+    def __len__(self) -> int:
+        return len(self.chain_key_to_index)
+
+    def __getitem__(self, index: int) -> Tuple[dict, str]:
+        # Take indexes unique to chain and return the complex data for that chain and the chain key.
+        chain_key = self.index_to_chain_key[index]
+        pdb_code = chain_key.split('-')[0]
+        return self.pdb_code_to_complex_data[pdb_code], chain_key
+
+    
+class ProteinAssemblyDataset(Dataset):
+    """
+    Dataset where every biological assembly is a separate index.
+    """
     def __init__(self, params):
         self.data = {}
-        for data in tqdm(os.listdir(params['dataset_path']), desc='Loading protein dataset.'):
-            path_to_data = os.path.join(params['dataset_path'], data)
+        for path_to_data in tqdm(get_list_of_all_paths(params['dataset_path']), desc='Loading protein dataset.'):
             pdb_prefix = path_to_data.rsplit('/', 1)[1]
             self.data[pdb_prefix] = torch.load(path_to_data)
         self.index_to_key = list(self.data.keys())
@@ -84,6 +183,3 @@ class UnclusteredProteinDataset(Dataset):
     def __getitem__(self, index: int) -> Tuple[dict, str]:
         pdb_code = self.index_to_key[index]
         return self.data[pdb_code], pdb_code
-    
-    def get_item_with_pdb_code(self, pdb_code: str) -> Tuple[dict, str]:
-        return self.data[pdb_code]
